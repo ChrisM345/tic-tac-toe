f1616174e9c66cea70986ebebcd8ad50b26e11e8
ChrisM345 <chrism.12277@gmail.com>
2024-03-07 19:21:06 -0600

    Started Gameboard and render

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+// Storing gameboard as an array inside of a Gameboard Object
+// Players will also be stored in objects
+// Object to control the flow of the game
+
+const Gameboard = (() => {
+    let gameboard = ['','','','','','','','',''];
+
+    const render = () => {
+        let boardGameHTML = '';
+        gameboard.forEach((square, idx) => {
+            boardGameHTML += `<div class="square" id=square-${idx}>${square}</div>`;
+        })
+        document.querySelector(".game-board").innerHTML = boardGameHTML;
+    }
+    
+    
+
+    return {
+        render,
+    }
+
+})();
+
