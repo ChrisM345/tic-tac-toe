937eb7a86f0e660eac93c259daa330845059ab56
ChrisM345 <chrism.12277@gmail.com>
2024-03-09 13:51:38 -0600

    Added players. Players can click squares. Logic to only allow square to be clicked once

--- scripts/script.js
+++ scripts/script.js
@@ -6,17 +6,68 @@ const Gameboard = (() => {
     let gameboard = ['','','','','','','','',''];
 
     const render = () => {
+        // Create the gameboard as an array of 9 empty strings.
+        // Create 9 divs and keep track of index in the ID for the div
         let boardGameHTML = '';
         gameboard.forEach((square, idx) => {
             boardGameHTML += `<div class="square" id=square-${idx}>${square}</div>`;
         })
         document.querySelector(".game-board").innerHTML = boardGameHTML;
+
+        document.querySelectorAll(".square").forEach((square) =>{
+            square.addEventListener('click', Game.handleClick)
+        })
     }
- 
+
+    const updateSquare = (square, mark) => {
+        document.querySelector(`#${square.id}`).innerText = mark
+        idx = square.id.split('-')[1]
+        gameboard[idx] = mark
+    }
+
+    const showBoard = () =>{
+        console.log(gameboard)
+    }
+
+
     return {
         render,
+        updateSquare,
+        showBoard,
     }
-
 })();
 
-Gameboard.render()
+const Game = (() => {
+    let players = [];
+    let currPlayerIndex;
+    const startGame = () => {
+        players = [
+            createPlayer(document.querySelector("#player-1").value, 'X'),
+            createPlayer(document.querySelector("#player-2").value, 'O')
+        ]
+
+        Gameboard.render()
+        currPlayerIndex = 0
+    }
+
+    const handleClick = (event) => {
+        if(event.target.innerText == ''){
+            Gameboard.updateSquare(event.target, players[currPlayerIndex].mark)
+            currPlayerIndex = currPlayerIndex == 0 ? 1 : 0
+        }
+    }
+
+    return {
+        startGame,
+        handleClick,
+    }
+})();
+
+function createPlayer (name, mark) {
+    return {
+        name,
+        mark
+    }
+}
+
+Game.startGame()
